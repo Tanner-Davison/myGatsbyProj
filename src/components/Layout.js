9c1bbb03b3d1545f280a0cfa1023fc917efe803f
Tanner Davison <tanner.davison95@gmail.com>
2024-01-30 00:23:27 -0700

    added some cool styled and svg

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+import media from 'styles/media';
+import colors from 'styles/colors';
+import text from 'styles/text';
+
+const Layout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Layout

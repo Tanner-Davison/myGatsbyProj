9e5f44aa6c05611ba69be9d9f70b004aef0dca8e
Tanner Davison <Tboydavison@gmail.com>
2024-01-29 22:23:09 -0700

    added a new site

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,27 @@
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-fonts",
+      options: {
+        fonts: [`Orbitron`, `Archivo`, `Roboto Mono`],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-alias-imports",
+      options: {
+        alias: {
+          components: "src/components",
+          styles: "src/styles",
+          utils: "src/utils",
+          // add more aliases here as needed
+        },
+        extensions: ["js", "jsx"],
+      },
+    },
+  ],
 };
